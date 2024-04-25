57c27347e357d0df3c3295cf369df3b7ed3e7604
Vktr22 <nds.viktoria@gmail.com>
2024-04-25 15:33:20 +0200

    reszlet

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-import { tesztesetek } from "./tesztek.js";
+import { masodfokuEgyenletMegoldasaTeszt } from "./tesztek.js";
+masodfokuEgyenletMegoldasaTeszt();
 
-console.log(masodfokuEgyenletMegoldasa(1,2,1))
+//console.log(masodfokuEgyenletMegoldasa(1,2,1))
 
 // Másodfokú egyenlet calculator online http://eprog.hu/oktatas/javascript/feladat05.html
 
 export function masodfokuEgyenletMegoldasa(a, b, c) {
+    if(a===0){
+        x1=""
+    }
+
     let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
     let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
     const eredmeny = {
--- tesztek.js
+++ tesztek.js
@@ -1,6 +1,57 @@
 import {masodfokuEgyenletMegoldasa} from "./main.js";
 
-export function tesztesetek(){
+export function masodfokuEgyenletMegoldasaTeszt(){
+    const tesztLista = [
+        {
+            a: 0,
+            b: 0,
+            c: 0,
+            vartX1: "Nincs megoldása az egyenletnek",
+            vartX2: "Nincs megoldása az egyenletnek"
+        },
+        {
+            a: 1,
+            b: 0,
+            c: 0,
+            vartX1: 0,
+            vartX2: "nincs"
+        },
+        {
+            a: 0,
+            b: 1,
+            c: 0,
+            vartX1: "Nincs megoldás, mert a diszkrimináns <=0",
+            vartX2: "Nincs megoldás, mert a diszkrimináns <=0"
+        },
+        {
+            a: 0,
+            b: 0,
+            c: 1,
+            vartX1: "Nincs megoldás, mert a diszkrimináns <=0",
+            vartX2: "Nincs megoldás, mert a diszkrimináns <=0"
+        },
+        {
+            a: 20,
+            b: 10,
+            c: 1,
+            vartX1: "Nincs megoldás, mert a diszkrimináns <=0",
+            vartX2: "Nincs megoldás, mert a diszkrimináns <=0"
+        },
+        
+    ]
+    tesztLista.forEach(function(teszt, index) {
+        const eredmeny = masodfokuEgyenletMegoldasa(teszt.a, teszt.b, teszt.c);
+        console.assert(
+            teszt.vartX1 === eredmeny.X1 &&
+            teszt.vartX2 === eredmeny.X2,
+            `HIBA! a/az ${index + 1}. tesztesetnél`)
+        /* 
+        
+        
+        ,"%o",
+        `a: ${masodfokuEgyenletMegoldasaTeszt.a} b: ${masodfokuEgyenletMegoldasaTeszt.b} c: ${masodfokuEgyenletMegoldasaTeszt.c} 
+        vartX1: ${masodfokuEgyenletMegoldasaTeszt.vartX1} vartX2: ${masodfokuEgyenletMegoldasaTeszt.vartX2} 
+        kapottX1: ${masodfokuEgyenletMegoldasa(x1)} kapottX2:  ${masodfokuEgyenletMegoldasa(x2)}`,`HIBA! a/az ${index +1}. tesztesetnél`) */
+    })
 
-    
 }
